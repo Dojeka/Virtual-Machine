a192dc0f955c0385ca49cb1626adb8116429caee
Dojeka <161097612+Dojeka@users.noreply.github.com>
2024-09-12 16:37:48 -0400

    Added RAM, registers, input buffer, output buffer, PC to CPU. Also added a decode and encode function.

--- CPU.java
+++ CPU.java
@@ -1,4 +1,29 @@
 import java.util.Scanner;
 
 public class CPU {
+    Scanner sc =new Scanner(System.in);
+
+    //64 bit RAM
+    private String[] RAM = new String[8];
+
+    //16 registers
+    private String[] registers = new String[16];
+    //reg-1 zero register
+    // reg-0 accumulator
+
+    private String InBuffer;
+    private String OutBuffer;
+    private int PC;
+
+    //takes hex and translates to base-10
+    public int decode(String input){
+        int base = Integer.parseInt(input,16);
+        return base;
+    }
+
+    //takes base-10 and translates to hex
+    public String encode(int input){
+        String hex = Integer.toHexString(input);
+        return hex;
+    }
 }
