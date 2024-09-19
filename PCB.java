c4a67e64324c066b2aaf0a3c739044358c5317ca
Isaac Lai <64813126+isaaclai30@users.noreply.github.com>
2024-09-19 09:51:54 -0400

    Adding PCB Class

--- PCB.java
+++ PCB.java
@@ -1,2 +1,44 @@
+
 public class PCB {
+    String[] instructionList;
+    int instructLength;
+    int priority;
+    String[] inputBuffer;
+    int inputLength;
+    String[] outputBuffer;
+    int outputLength;
+    public PCB (String[] instructionList, int instructLength, int priority, String[] inputBuffer, int inputLength, String[] outputBuffer, int outputLength) {
+        this.instructionList = instructionList;
+        this.priority = priority;
+        this.inputBuffer = inputBuffer;
+        this.inputLength = inputLength;
+        this.outputBuffer = outputBuffer;
+        this.outputLength = outputLength;
+    }
+
+    public String getLine (int line) {
+        return instructionList[line];
+    }
+    public String getInputLine(int line) {
+        return inputBuffer[line];
+    }
+    public void setOutputBuffer(int line, String output) {
+        outputBuffer[line] = output;
+    }
+
+
+    // returns entire arrays for writing into file or reading in console
+    public String[] getInputBuffer() {
+        return inputBuffer;
+    } // returns entire inputBuffer
+    public String[] getOutputBuffer() {
+        return outputBuffer;
+    } // returns entire outputBuffer
+    public String[] getInstructionList() {
+        return instructionList;
+    } // returns entire instructionList
+
+    public int getLength() {
+        return instructLength + inputLength + outputLength;
+    }
 }
