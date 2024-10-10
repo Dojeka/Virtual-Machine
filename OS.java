f0da014d13dd5390f47be1e0e9de58138a763b79
Dojeka <161097612+Dojeka@users.noreply.github.com>
2024-10-10 13:13:09 -0400

    Update OS.java
    Added RAM

--- OS.java
+++ OS.java
@@ -1,6 +1,7 @@
 public class OS {
     String [] disk = new String[2048];
-
+    String [] RAM = new String[1024];
+    
     public static void main(String[] args) {
         String[] ex = {"C050005C" , "4B060000", "4B010000"};
         CPU cpu = new CPU(ex);
