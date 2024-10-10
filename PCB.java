7ce94a86892983d52d9738dc6820126a0fa0a68c
Isaac Lai <64813126+isaaclai30@users.noreply.github.com>
2024-10-10 13:17:31 -0400

    PCB getter and setters

--- PCB.java
+++ PCB.java
@@ -24,9 +24,6 @@ public class PCB {
         this.jobEndingInDisk = jobEndingInDisk; //exclusive
     } //PCB Constructor
 
-    //Added by John
-    public PCB() {}
-
     //Added job info of jobs space in ram to pcb
     public void setJobBeginningInRam(int jobBeginningInRam) {
         this.jobBeginningInRam = jobBeginningInRam;
@@ -36,6 +33,31 @@ public class PCB {
     }
     public void setJobInputBufferStartInRam(int jobInputBufferStartInRam) {this.jobInputBufferStartInRam = jobInputBufferStartInRam;}
     public void setJobOutputBufferStartInRam(int jobOutputBufferStartInRam) {this.jobOutputBufferStartInRam = jobOutputBufferStartInRam;}
+    public void setJobBeginningInDisk(int jobBeginningInDisk) {
+        this.jobBeginningInDisk = jobBeginningInDisk;
+    }
+
+    public void setJobEndingInDisk(int jobEndingInDisk) {
+        this.jobEndingInDisk = jobEndingInDisk;
+    }
+    public int getJobBeginningInDisk() {
+        return jobBeginningInDisk;
+    }
+    public int getJobEndingInDisk() {
+        return jobEndingInDisk;
+    }
+    public int getJobBeginningInRam() {
+        return jobBeginningInRam;
+    }
+    public int getJobInputBufferStartInRam() {
+        return jobInputBufferStartInRam;
+    }
+    public int getJobOutputBufferStartInRam() {
+        return jobOutputBufferStartInRam;
+    }
+    public int getJobEndingInRam() {
+        return jobEndingInRam;
+    }
 
     public int getPriority() {
         return priority;
